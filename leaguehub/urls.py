0ce4245f888bc38bdd4b8d94f37ae4a4491f097f
Morgan Stenberg <morganstenberg@gmail.com>
2024-01-25 15:21:47 +0000

    install summernote

--- leaguehub/urls.py
+++ leaguehub/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include ('league.urls')),
     path("accounts/", include("allauth.urls")),
+    path('summernote/', include('django_summernote.urls')),
 ]
