4770eb2248d3e036d9978adc9f54b2afdd5d780d
Morgan Stenberg <morganstenberg@gmail.com>
2024-02-15 17:20:11 +0000

    remove sumernote, update search fields and for admin

--- league/admin.py
+++ league/admin.py
@@ -1,18 +1,16 @@
 from django.contrib import admin
-from django_summernote.admin import SummernoteModelAdmin
 from .models import League, Match
 
 # Register your models here.
 
 @admin.register(League)
-class LeagueAdmin(SummernoteModelAdmin):
-
+class LeagueAdmin(admin.ModelAdmin):
     list_display = ('name', 'slug', 'league_creator')
-    search_fields = ['name', 'league_creator']
+    search_fields = ['name', 'league_creator__username']
     prepopulated_fields  = {'slug': ('name',)}
-    summernote_field = ('description')
+    autocomplete_fields = ['league_members']
 
 @admin.register(Match)
 class MatchAdmin(admin.ModelAdmin):
     list_display = ('league', 'date', 'home_team', 'away_team', 'home_team_score', 'away_team_score')
-    search_fields = ['home_team', 'away_team']
+    search_fields = ['home_team__username', 'away_team__username']
