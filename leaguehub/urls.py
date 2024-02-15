2c36cbbc686279998e0cbd297321b13377720e82
Morgan Stenberg <morganstenberg@gmail.com>
2024-02-15 17:49:35 +0000

    clean upp css rules, ststyle list of matches

--- leaguehub/urls.py
+++ leaguehub/urls.py
@@ -21,5 +21,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include ('league.urls')),
     path("accounts/", include("allauth.urls")),
-    path('summernote/', include('django_summernote.urls')),
 ]
