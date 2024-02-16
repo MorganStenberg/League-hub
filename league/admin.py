707bbf5e69120a990e69aa254229aa07dc7fd56e
Morgan Stenberg <morganstenberg@gmail.com>
2024-02-16 09:53:55 +0000

    add docstring to admin, add table of contents to readme

--- league/admin.py
+++ league/admin.py
@@ -5,6 +5,11 @@ from .models import League, Match
 
 @admin.register(League)
 class LeagueAdmin(admin.ModelAdmin):
+    """
+    List fields for display in admin interface, 
+    fields that are searchable, and fields to prepopulate. 
+    And autocomplete fields.
+    """
     list_display = ('name', 'slug', 'league_creator')
     search_fields = ['name', 'league_creator__username']
     prepopulated_fields  = {'slug': ('name',)}
@@ -12,5 +17,9 @@ class LeagueAdmin(admin.ModelAdmin):
 
 @admin.register(Match)
 class MatchAdmin(admin.ModelAdmin):
+    """
+    List fields for display in admin interface and
+    fields that are searchable.
+    """
     list_display = ('league', 'date', 'home_team', 'away_team', 'home_team_score', 'away_team_score')
     search_fields = ['home_team__username', 'away_team__username']
